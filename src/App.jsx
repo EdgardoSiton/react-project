10f96480db9d7e9a778d7d443d1d8512a599b0c3
Edgardo <edgardositon92@gmail.com>
2024-07-17 12:48:20 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import Student from "./components/Student";
 
-function App() {
-  const [count, setCount] = useState(0)
+const students = [
+  {
+    name: "Edgardo A.Siton",
+    department: "Science",
+    finalGrade: 85,
+    status: "Pass",
+  },
+  {
+    name: "SweetVenice Casia",
+    department: "Arts",
+    finalGrade: 92,
+    status: "Pass",
+  },
+  {
+    name: "Rodel Celis",
+    department: "Commerce",
+    finalGrade: 76,
+    status: "Pass",
+  },
+  {
+    name: "Darla Kayla Mayono Ipon",
+    department: "Science",
+    finalGrade: 88,
+    status: "Pass",
+  },
+  {
+    name: "Aeron VillaFuerte",
+    department: "Arts",
+    finalGrade: 67,
+    status: "Pass",
+  },
+  {
+    name: "Chisler Uy",
+    department: "Commerce",
+    finalGrade: 90,
+    status: "Pass",
+  },
+  {
+    name: "Jie Navvarro",
+    department: "Science",
+    finalGrade: 83,
+    status: "Pass",
+  },
+  {
+    name: "David SalonPas",
+    department: "Arts",
+    finalGrade: 78,
+    status: "Pass",
+  },
+  {
+    name: "Sara Geronimos",
+    department: "Commerce",
+    finalGrade: null,
+    status: "Fail",
+  },
+  {
+    name: "Doctor Strange",
+    department: "Science",
+    finalGrade: null,
+    status: "Fail",
+  },
+];
 
+const Students = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <table>
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>Student Names</th>
+          <th>Department</th>
+          <th>Final Grade</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {students.map((student, index) => (
+          <Student key={index} student={student} index={index} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
-export default App
+export default Students;
